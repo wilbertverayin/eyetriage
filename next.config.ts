9d48703d66d8d0bfa628ec834d91929d7eda9046
Wilbert Verayin <wilbert@dataspark.nl>
2026-06-25 21:27:22 +0800

    chore: configure custom domain eyetriage.visibleclinic.com

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: process.env.GITHUB_PAGES === 'true' ? '/eyetriage' : '',
   typescript: {
     ignoreBuildErrors: true,
   },
