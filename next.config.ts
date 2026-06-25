349cd12b5a5525b9945839df50d47c5cb9308cb5
Wilbert Verayin <wilbertverayin@gmail.com>
2026-06-25 19:59:49 +0800

    chore: initialize Next.js 15 project (eye health triage platform)

---
+++ next.config.ts
@@ -0,0 +1,48 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  basePath: process.env.GITHUB_PAGES === 'true' ? '/eyetriage' : '',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.ko-fi.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'maps.googleapis.com',
+        port: '',
+        pathname: '/maps/api/place/photo**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'places.googleapis.com',
+        port: '',
+        pathname: '/v1/**/media**',
+      },
+    ],
+  },
+  experimental: {
+    // allowedDevOrigins: ['https://6000-firebase-studio-1749361246774.cluster-sumfw3zmzzhzkx4mpvz3ogth4y.cloudworkstations.dev'],
+  },
+};
+
+export default nextConfig;
